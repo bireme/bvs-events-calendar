edbf9f810021a0dbaa73319893cf26fb1d5d18bd
Wilson Moura <wilsonsmoura@gmail.com>
2016-04-27 17:45:43 -0300

    Fixed #20 -- Create default template to show up Event custom post type metadata

--- wp-themes/bvs-eventos/functions.php
+++ wp-themes/bvs-eventos/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 	register_nav_menus( array(
 		'top_menu' => 'Top Menu',
 	) );
@@ -61,4 +62,183 @@
         remove_filter( 'excerpt_more', 'twentysixteen_excerpt_more' ); 
     }
     add_action( 'after_setup_theme', 'child_theme_setup' );
+
+    function get_video_data($url) {
+        $args = array();
+        if (strpos($url, 'youtube') > 0) {
+            $parts = parse_url($url);
+            parse_str($parts['query'], $query);
+            $args['embed'] = "https://www.youtube.com/embed/" . $query['v'];
+            $args['type']  = 'youtube';
+            $args['id']    = $query['v'];
+
+            return $args;
+        } elseif (strpos($url, 'vimeo') > 0) {
+            $parts = parse_url($url);
+            $id  = end(split('/', $parts['path']));
+            $args['embed'] = "https://player.vimeo.com/video/" . $id;
+            $args['type']  = 'vimeo';
+            $args['id']    = $id;
+
+            return $args;
+        } else {
+            return false;
+        }
+    }
+
+    function slideshare_embed($url, $output = 'json') {
+        $api_request = "http://www.slideshare.net/api/oembed/2?url=" . $url . "&format=" . $output;
+
+        if ( $output == 'json' ) {
+            $response = file_get_contents($api_request);
+            if ($response){
+                $obj = json_decode($response);
+                $html = $obj->html;
+
+                $doc = new DOMDocument();
+                $doc->loadHTML($html);
+                $iframes = $doc->getElementsByTagName('iframe');
+
+                foreach($iframes as $node) {
+                    $src = $node->getAttribute('src');
+                }
+
+                return $src;
+            }
+        } elseif ( $output == 'xml' ) {
+            $xml = simplexml_load_file($api_request);
+            if ( $xml ) {
+                $doc = new DOMDocument();
+                $doc->loadHTML($xml->html);
+                $iframes = $doc->getElementsByTagName('iframe');
+
+                foreach($iframes as $node) {
+                    $src = $node->getAttribute('src');
+                }
+
+                return $src;
+            }
+        }
+    }
+
+    function add_theme_scripts() {
+        wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false', array(), '3', true );
+        wp_enqueue_script( 'google-map-init', get_stylesheet_directory_uri() . '/js/google-maps.js', array('google-map', 'jquery'), '0.1', true );
+    }
+    add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+    function event_nav_menu_meta_box_markup($obj) {
+        wp_nonce_field(basename(__FILE__), "event-nav-menu-nonce");
+
+        ?>
+            <div>
+                <p><strong><?php _e( 'Menu', 'bvseventos' ); ?></strong></p>
+                <label class="screen-reader-text" for="event-nav-menu"><?php _e( 'Menu', 'bvseventos' ); ?></label>
+                <select name="event-nav-menu" id="event-nav-menu">
+                    <?php 
+                        $menus = get_terms( 'nav_menu', array( 'hide_empty' => true ) );
+                        ?>
+                            <option></option>
+                        <?php
+                        foreach($menus as $menu) {
+                            if($menu->term_id == get_post_meta($obj->ID, "event_nav_menu", true)) {
+                                ?>
+                                    <option value="<?php echo $menu->term_id; ?>" selected><?php echo $menu->name; ?></option>
+                                <?php    
+                            } else {
+                                ?>
+                                    <option value="<?php echo $menu->term_id; ?>"><?php echo $menu->name; ?></option>
+                                <?php
+                            }
+                        }
+                    ?>
+                </select>
+            </div>
+        <?php  
+    }
+
+    function event_nav_menu_meta_box() {
+        add_meta_box("event-nav-menu-meta-box", __( "Navigation Menu", "bvseventos" ), "event_nav_menu_meta_box_markup", "event", "side", "high", null);
+    }
+    add_action("add_meta_boxes", "event_nav_menu_meta_box");
+
+    function save_event_nav_menu_meta_box($post_id, $post, $update) {
+        if (!isset($_POST["event-nav-menu"]) || !wp_verify_nonce($_POST["event-nav-menu-nonce"], basename(__FILE__)))
+            return $post_id;
+
+        if(!current_user_can("edit_post", $post_id))
+            return $post_id;
+
+        if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+            return $post_id;
+
+        if('event' != $post->post_type)
+            return $post_id;
+
+        $event_nav_menu = "";
+
+        if(isset($_POST["event-nav-menu"])) {
+            $event_nav_menu = $_POST["event-nav-menu"];
+        }   
+        update_post_meta($post_id, "event_nav_menu", $event_nav_menu);
+    }
+    add_action("save_post", "save_event_nav_menu_meta_box", 10, 3);
+
+    function event_nav_menu( $event_menu, $homepage ) {
+        global $post;
+
+        if ( $event_menu ) :
+
+            wp_nav_menu( array(
+                'menu' => $event_menu,
+                'container_id' => 'event-menu'
+            ) );
+
+        elseif ( !$homepage && !is_page() ) :
+
+            $schedule   = '';
+            $location   = get_field('location');
+            $start_date = date("d/m/Y", strtotime(get_field('start_date')));
+
+            $args = array(
+                'post_type' => 'schedule',
+                'post_status' => 'publish',
+                'posts_per_page' => 1,
+                'meta_query' => array(
+                    array(
+                        'key' => 'event',
+                        'value' => serialize(strval($post->ID)),
+                        'compare' => 'LIKE'
+                    )
+                )
+            );
+
+            $query = null;
+            $query = new WP_Query($args);
+
+            if ( $query->have_posts() )
+                $schedule = get_permalink( $query->post->ID );
+        ?>
+
+            <div id="event-menu">
+                <ul>
+                    <li><a href="<?php echo get_settings('home'); ?>">Home</a></li>
+                    <?php if ( $schedule ) : ?>
+                        <li><a href="<?php echo $schedule; ?>"><?php _e( 'Schedule', 'bvseventos' ); ?></a></li>
+                    <?php endif; ?>
+                    <?php if ( $start_date ) : ?>
+                        <li><a href="#date"><?php _e( 'Date', 'bvseventos' ); ?></a></li>
+                    <?php endif; ?>
+                    <?php if ( $post->post_content || $post->post_excerpt ) : ?>
+                        <li><a href="#event-content"><?php _e( 'Description', 'bvseventos' ); ?></a></li>
+                    <?php endif; ?>
+                    <?php if ( $location ) : ?>
+                        <li><a href="#location"><?php _e( 'Location', 'bvseventos' ); ?></a></li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+
+        <?php endif;
+    }
+
 ?>
--- wp-themes/bvs-eventos/single-event.php
+++ wp-themes/bvs-eventos/single-event.php
@@ -9,41 +9,84 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	AAA
+<?php
+	$homepage   = get_field( 'homepage' );
+	$event_menu = get_field( 'event_nav_menu' );
+?>
+
+<div id="primary" class="content-area event-page">
 	<main id="main" class="site-main" role="main">
+		<div class="breadcrumb">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">home</a> / <strong><?php single_post_title(); ?></strong>
+		</div>
+
 		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+			if ( $homepage ) :
 
-			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
+				$post = $homepage[0];
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-
-			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
-				the_post_navigation( array(
-					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
-				) );
-			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-			}
-
-			// End of the loop.
-		endwhile;
+				// Include the single page content template.
+				get_template_part( 'template-parts/content', 'page' );
+			
+			else :
 		?>
+
+			<?php
+				$venue = get_field('venue');
+				$location = get_field('location');
+				$start_date = date("d/m/Y", strtotime(get_field('start_date')));
+				$end_date = (get_field('end_date') && get_field('start_date') != get_field('end_date')) ? ' - ' . date("d/m/Y", strtotime(get_field('end_date'))) : '';
+			?>
+
+			<div class="event-title">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</div>
+
+			<?php event_nav_menu( $event_menu, $homepage ); ?>
+
+			<?php if ( ! empty( $start_date ) ) : ?>
+				<div id="date" class="event-date">
+					<h3 class="event-header"><?php _e( 'Date', 'bvseventos' ); ?></h3>
+					<?php echo $start_date . $end_date; ?>
+				</div>
+			<?php endif; ?>
+
+			<div id="event-content" class="event-content">
+				<h3 class="event-header"><?php _e( 'Description', 'bvseventos' ); ?></h3>
+				<?php
+					if ( $post->post_content ) {
+                        echo wpautop($post->post_content);
+                    } elseif ( get_the_excerpt() ) {
+                        the_excerpt();
+                    }
+                ?>
+			</div>
+			
+			<?php if ( ! empty( $location ) ) : ?>
+				<div id="location" class="event-location">
+					<h3 class="event-header"><?php _e( 'Location', 'bvseventos' ); ?></h3>
+					<span>
+						<?php
+							if ( ! empty( $venue ) )
+								echo $venue;
+							else
+								echo $location['address'];
+						?>
+					</span>
+					<div class="acf-map">
+						<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+					</div>
+				</div>
+			<?php endif; ?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+			?>
+
+		<?php endif; ?>
 
 	</main><!-- .site-main -->
 
@@ -51,5 +94,6 @@ get_header(); ?>
 
 </div><!-- .content-area -->
 
-<?php get_sidebar(); ?>
+<?php //if ( ! $homepage ) { get_sidebar(); } ?>
+
 <?php get_footer(); ?>
