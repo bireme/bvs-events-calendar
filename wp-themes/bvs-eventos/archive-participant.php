14ea6b408d0a5006be39d8ae0c624a1b41dedb39
Wilson Moura <wilsonsmoura@gmail.com>
2018-11-28 19:23:14 -0200

    Update archive-participant.php

--- wp-themes/bvs-eventos/archive-participant.php
+++ wp-themes/bvs-eventos/archive-participant.php
@@ -58,6 +58,9 @@ get_header(); ?>
         ?>
 
         </main><!-- .site-main -->
+
+        <?php get_sidebar( 'content-bottom' ); ?>
+
     </div><!-- .content-area -->
 
 <?php get_sidebar(); ?>
